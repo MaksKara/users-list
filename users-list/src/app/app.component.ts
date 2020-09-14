87ec113fe9539ff332b8dbbe7fedf761ee8ff8c9
MaksKara <62358244+MaksKara@users.noreply.github.com>
2020-09-14 23:54:33 +0300

    lesson 2

--- users-list/src/app/app.component.ts
+++ users-list/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'users-list';
+  name = 'Список пользователей';
   typesOfShoes: string[] = ['Johny','Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
