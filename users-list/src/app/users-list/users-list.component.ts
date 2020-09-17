071dca0183a0c78df7385779eb78b164b1c7b7aa
MaksKara <62358244+MaksKara@users.noreply.github.com>
2020-09-18 00:47:49 +0300

    lesson 5

--- users-list/src/app/users-list/users-list.component.ts
+++ users-list/src/app/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {UsersService} from './../users.service';
-import {User} from './../shared/user';
+import { UsersService } from './../users.service';
+import { User } from './../shared/user';
+import { MatListOption } from '@angular/material/list';
 
 @Component({
   selector: 'app-users-list',
@@ -10,7 +11,10 @@ import {User} from './../shared/user';
 export class UsersListComponent implements OnInit {
 
   usersList: User[] = [];
-  tempValue = 0;
+  username: string;
+  name: string;
+  role: string;
+  selectedList: User[];
 
   constructor(public userService: UsersService) { }
 
@@ -24,6 +28,32 @@ export class UsersListComponent implements OnInit {
   
   sort(direction: string) {
  	this.usersList = this.userService.sortUsers(direction);
+  }
+
+  addUser() {
+  	this.userService.addUser({
+    id: Math.floor((Math.random() * 6) + 10),
+    name: this.name,
+    username: this.username,
+    email: "",
+    role: this.role,
+    phone: "",
+    website: ""
+  	});
+
+  	this.usersList = this.userService.getUsersList();
+  }
+
+  selectItem(users: MatListOption[]) {
+  	this.selectedList = [];
+  	users.forEach(element => {
+  		this.selectedList.push(element.value);
+  	});
+  }
+
+  deleteUsers() {
+  	this.userService.deleteUsers(this.selectedList);
+  	this.usersList = this.userService.getUsersList();
   }
 
 }
